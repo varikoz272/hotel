9da8f0fe3eb3db1b2a105f94023f17a5d0410be0
varikoz272 <vornickon@gmail.com>
2025-02-18 18:51:28 +0300

    initial docker commit

--- web/hotel-front/vite.config.js
+++ web/hotel-front/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+  }
 })
